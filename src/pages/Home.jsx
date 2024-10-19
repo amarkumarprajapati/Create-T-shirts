90c22907018fdd7aea48f71d5e636cd832d8c938
Amar Kumar Prajapati <100253987+amarkumarprajapati@users.noreply.github.com>
2024-10-19 21:47:31 +0530

    removed packages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,28 @@
-import { motion, AnimatePresence } from 'framer-motion';
-import { useSnapshot } from 'valtio';
-import Typed from 'react-typed';
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
 import React, { useEffect } from "react";
 
-
-import state from '../store';
-import { CustomButton } from '../components';
+import state from "../store";
+import { CustomButton } from "../components";
 
 import {
   headContainerAnimation,
   headContentAnimation,
   headTextAnimation,
-  slideAnimation
-} from '../config/motion';
+  slideAnimation,
+} from "../config/motion";
 
 const Home = () => {
   const snap = useSnapshot(state);
-   
+
   return (
     <AnimatePresence>
       {snap.intro && (
-        <motion.section className="home" {...slideAnimation('left')}>
+        <motion.section className="home" {...slideAnimation("left")}>
           <motion.header {...slideAnimation("down")}>
-            <img 
-              src='./1.webp'
-              height='100px'
+            <img
+              src="./1.webp"
+              height="100px"
               alt="logo"
               className="w-8 h-8 object-contain"
             />
@@ -33,52 +31,29 @@ const Home = () => {
           <motion.div className="home-content" {...headContainerAnimation}>
             <motion.div {...headTextAnimation}>
               <h6 className="head-text">
-            Start
-                 <br className="xl:block hidden" /> 
-                 
-                 <Typed
-                  strings={[
-                    "Design"
-                  ]}
-                  typeSpeed={200}
-                  backSpeed={100}
-                  loop
-              />
+                Start
+                <br className="xl:block hidden" />
+                Design
               </h6>
-              </motion.div>
-             <motion.div
+            </motion.div>
+            <motion.div
               {...headContentAnimation}
-              className="flex flex-col gap-5"
-            >
+              className="flex flex-col gap-5">
               <p className="max-w-md font-normal text-gray-600 text-base">
-              <Typed
-                  strings={[
-                    "Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your imagination</strong> and define your own style"
-                  ]}
-                  typeSpeed={5}
-                  backSpeed={5}
-                  loop
-              />
+                Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your imagination</strong> and define your own style.
               </p>
-              <CustomButton 
+              <CustomButton
                 type="filled"
                 title="Customize It"
-                handleClick={() => state.intro = false}
+                handleClick={() => (state.intro = false)}
                 customStyles="w-fit px-4 py-2.5 font-bold text-sm"
               />
             </motion.div>
           </motion.div>
         </motion.section>
-      
-
-
       )}
     </AnimatePresence>
-  )
-}
+  );
+};
 
-
-
-
-
-export default Home
+export default Home;
